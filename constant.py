b0d0e63769859cd2caa4934d1f22f4c9d55f9d46
MuhammadAfaqShuaib <afaq.shoaib09@gmail.com>
2022-07-19 00:18:58 +0500

    version 1.0: Automated Hangman Completed

---
+++ constant.py
@@ -0,0 +1,6 @@
+''' Constant declared used for automated hangman game program '''
+
+TEST_CASES_COUNT = 10
+MAX_ATTEMPTS = 7
+FILE_NAME = 'dictionary.txt'
+HANGMAN_API_URL = 'https://hangman-api.herokuapp.com/hangman'
